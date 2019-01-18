4225f388d0920e3e3a0eb6d8ec5375e5ff5ee0b2
Lucas Fernando Amorim <lf.amorim@yahoo.com.br>
2019-01-18 17:22:41 -0200

    fix

--- src/js/internals/modules/icheques.js
+++ src/js/internals/modules/icheques.js
@@ -3,7 +3,7 @@ import oneTime from 'one-time';
 module.exports = controller => {
     const kronoosCall = oneTime(args => $.getScript('/js/kronoos.js', () => controller.trigger('kronoos::init', args)));
     const refinCall = oneTime(() => $.getScript('https://cdn.jsdelivr.net/npm/harlan-icheques-refin@1.0.5/index.js'));
-    const veiculosCall = oneTime(() => $.getScript('https://cdn.jsdelivr.net/npm/harlan-icheques-veiculos@1.1.1/index.js'));
+    const veiculosCall = oneTime(() => $.getScript('https://cdn.jsdelivr.net/npm/harlan-icheques-veiculos@1.1.3/index.js'));
     if (!controller.confs.icheques.hosts.includes(document.location.hostname)) return;
 
     controller.registerBootstrap('icheques::init::plataform', callback => $.getScript('/js/icheques.js', () => {
