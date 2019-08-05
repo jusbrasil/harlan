81230542b3f3e62ce0606834671e0c503cdf8776
Lucas Fernando Amorim <lf.amorim@yahoo.com.br>
2019-08-04 22:54:27 -0300

    follow call

--- src/js/internals/modules/icheques.js
+++ src/js/internals/modules/icheques.js
@@ -3,20 +3,16 @@ import oneTime from 'one-time';
 module.exports = controller => {
     const refinCall = oneTime(() => $.getScript('https://cdn.jsdelivr.net/npm/harlan-icheques-refin@1.0.9/index.js'));
     const veiculosCall = oneTime(() => $.getScript('https://cdn.jsdelivr.net/npm/harlan-icheques-veiculos@1.1.3/index.js'));
+    const followCall = oneTime(() => $.getScript('https://cdn.jsdelivr.net/npm/harlan-follow-document@1.1.5/index.js'));
     if (!controller.confs.icheques.hosts.includes(document.location.hostname)) return;
 
     controller.registerBootstrap('icheques::init::plataform', callback => $.getScript('/js/icheques.js', () => {
-
+        callback();
         if (navigator.userAgent.match(/iPad/i) !== null) {
-            callback();
             return;
         }
-
-        callback();
-
-        if (!(controller.confs.user.tags && 
-                controller.confs.user.tags.indexOf('no-refin') !== -1)) refinCall();
-        if (!(controller.confs.user.tags && 
-                controller.confs.user.tags.indexOf('no-veiculos') !== -1)) veiculosCall();
+        followCall();
+        if (!(controller.confs.user.tags && controller.confs.user.tags.indexOf('no-refin') !== -1)) refinCall();
+        if (!(controller.confs.user.tags && controller.confs.user.tags.indexOf('no-veiculos') !== -1)) veiculosCall();
     }));
 };
