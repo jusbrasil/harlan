ed54e0a9281adc9927bf9098cc2c979f7c0979d9
Lucas Fernando Amorim <lf.amorim@yahoo.com.br>
2019-08-06 15:32:50 -0300

    feature: force

--- src/js/internals/modules/icheques.js
+++ src/js/internals/modules/icheques.js
@@ -1,10 +1,11 @@
 import oneTime from 'one-time';
 
 module.exports = controller => {
+    if (!controller.confs.icheques.hosts.includes(document.location.hostname)) return;
+
     const refinCall = oneTime(() => $.getScript('https://cdn.jsdelivr.net/npm/harlan-icheques-refin@1.0.9/index.js'));
     const veiculosCall = oneTime(() => $.getScript('https://cdn.jsdelivr.net/npm/harlan-icheques-veiculos@1.1.3/index.js'));
     const followCall = oneTime(() => $.getScript('https://cdn.jsdelivr.net/npm/harlan-follow-document@1.1.5/index.js'));
-    if (!controller.confs.icheques.hosts.includes(document.location.hostname)) return;
 
     controller.registerBootstrap('icheques::init::plataform', callback => $.getScript('/js/icheques.js', () => {
         callback();
