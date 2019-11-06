0311ab0ee7fa2fd80518019a49f1b38bc29b7289
Davi Almeida <davi@pop-os.localdomain>
2019-11-06 11:21:06 -0300

    .

--- src/js/internals/modules/icheques.js
+++ src/js/internals/modules/icheques.js
@@ -12,7 +12,7 @@ module.exports = controller => {
     const followCall = oneTime(() => $.getScript('https://cdn.jsdelivr.net/npm/harlan-icheques-follow-document@1.2.20/index.min.js').fail(failAlert));
     const statuspageCall = oneTime(() => $.getScript('https://cdn.jsdelivr.net/npm/harlan-icheques-statuspage/index.js').fail(failAlert));
     const pdfMonitoramento = oneTime(() => $.getScript('https://cdn.jsdelivr.net/npm/harlan-icheques-monitoramento-pdf@1.0.21/index.js').fail(failAlert));
-    const relatorioAnalitico = oneTime(() => $.getScript('https://cdn.jsdelivr.net/npm/harlan-icheques-relatorio-analitico@1.0.5/index.js').fail(failAlert));
+    const relatorioAnalitico = oneTime(() => $.getScript('https://cdn.jsdelivr.net/npm/harlan-icheques-relatorio-analitico@1.0.6/index.js').fail(failAlert));
     controller.registerBootstrap('icheques::init::plataform', callback => $.getScript('/js/icheques.js').done(() => {
         callback();
         const tags = (controller.confs.user || {}).tags || [];
